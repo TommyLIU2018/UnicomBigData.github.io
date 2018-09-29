5b85bf8ae24334765e926ebd485c610883f7a4d6
xingwGitHub <18404972905@163.com>
2018-09-29 15:08:08 +0800

    tijiao

---
+++ src/pages/boot-page/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="boot-page">
+    <navBar></navBar>
+    <div><img :src="imgsrc"/></div>
+  </div>
+</template>
+
+<script>
+import navBar from 'components/navBar/navBar';
+import imgurl from '../../../static/bg.jpeg'
+export default {
+  name: 'bootPage',
+  data () {
+    return {
+      msg: 'Welcome to 金融小场景',
+      imgsrc: imgurl
+    }
+  }, 
+  components:{
+    navBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "./../../assets/css/variable.scss";
+@import "./../../assets/css/mixin.scss";
+
+
+.boot-page {
+  width: 1280px;
+  margin: auto;
+  color: #000
+}
+img {
+  width: 100%;
+}
+</style>
